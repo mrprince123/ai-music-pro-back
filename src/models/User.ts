98f8c8f0bbe676c527988781547be078593142ac
mrprince123 <princekrdss2018@gmail.com>
2026-04-08 01:36:15 +0530

    Feat: fixed some minor bugs and backend is ready for now

--- src/models/User.ts
+++ src/models/User.ts
@@ -24,9 +24,9 @@ const userSchema: Schema = new Schema({
 });
 
 // Hash password before saving
-userSchema.pre<IUser>('save', async function (next) {
+userSchema.pre('save', async function (this: any) {
     if (!this.isModified('password')) {
-        next();
+        return;
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
